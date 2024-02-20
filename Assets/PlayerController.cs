3c44567a628bda6f58cd9e9f604a67249eb4991f
petak5 <peterurgos12345@gmail.com>
2024-02-20 10:31:20 +0200

    Move player to mouse position on click

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,18 +6,23 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Camera camera;
+    
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        agent.SetDestination(new Vector3(-1, 2, 1));
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            agent.SetDestination(camera.ScreenToWorldPoint(Input.mousePosition));
+        }
     }
 }
