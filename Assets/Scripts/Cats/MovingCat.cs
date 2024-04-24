977b9fbaf646a43ad4657b7a92f492994830bba3
Akemi <ake3desuyo@gmail.com>
2024-04-24 23:10:37 +0300

    changed script from feedback
    ・classes inside the namespace indented to the right
    ・moved CatsAnimation script to the Cats file

--- Assets/Scripts/Cats/MovingCat.cs
+++ Assets/Scripts/Cats/MovingCat.cs
@@ -4,69 +4,69 @@ using UnityEngine;
 
 namespace Cats
 {
-public class MovingCat : MonoBehaviour
-{
-    [SerializeField] private Transform player;
-    [SerializeField] private float speed = 1f;
-
-    private Vector2 firstPosition;
-    public Transform secondPosition;
-
-    private Vector2 targetPosition;
-    public bool isMovingToPlayer = false;
-    private bool isMovingToSecondPosition;
-
-    private void Start()
+    public class MovingCat : MonoBehaviour
     {
-        firstPosition = transform.position;
-        targetPosition = secondPosition.position;
-        isMovingToSecondPosition = true;
-    }
+        [SerializeField] private Transform player;
+        [SerializeField] private float speed = 1f;
 
-    void Update()
-    {
-        if (!isMovingToPlayer)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, targetPosition) < 0.1)
-            {
-                SwitchDirection();
-                // TODO: sleep for a while?
-            }
-        }
-    }
+        private Vector2 firstPosition;
+        public Transform secondPosition;
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        //isMovingToPlayer = true;
+        private Vector2 targetPosition;
+        public bool isMovingToPlayer = false;
+        private bool isMovingToSecondPosition;
 
-        if(collision.tag == "Player")
+        private void Start()
         {
-            isMovingToPlayer = true;
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            isMovingToPlayer = false;
-        }
-    }
-
-    private void SwitchDirection()
-    {
-        if (isMovingToSecondPosition)
-        {
-            targetPosition = firstPosition;
-            isMovingToSecondPosition = false;
-        }
-        else
-        {
+            firstPosition = transform.position;
             targetPosition = secondPosition.position;
             isMovingToSecondPosition = true;
         }
+
+        void Update()
+        {
+            if (!isMovingToPlayer)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, targetPosition) < 0.1)
+                {
+                    SwitchDirection();
+                    // TODO: sleep for a while?
+                }
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            //isMovingToPlayer = true;
+
+            if (collision.tag == "Player")
+            {
+                isMovingToPlayer = true;
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
+            {
+                isMovingToPlayer = false;
+            }
+        }
+
+        private void SwitchDirection()
+        {
+            if (isMovingToSecondPosition)
+            {
+                targetPosition = firstPosition;
+                isMovingToSecondPosition = false;
+            }
+            else
+            {
+                targetPosition = secondPosition.position;
+                isMovingToSecondPosition = true;
+            }
+        }
     }
-}
 }
--- Assets/Scripts/Cats/StationaryCat.cs
+++ Assets/Scripts/Cats/StationaryCat.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 
 namespace Cats
 {
-public class StationaryCat : MonoBehaviour
-{
-    [SerializeField] private Transform player;
-    [SerializeField] private float attackSpeed = 0.8f;
-
-    private Vector2 startPosition;
-    private bool isMovingToStart = false;
-
-    private void Start()
+    public class StationaryCat : MonoBehaviour
     {
-        startPosition = transform.position;
-    }
+        [SerializeField] private Transform player;
+        [SerializeField] private float attackSpeed = 0.8f;
 
-    private void Update()
-    {
-        if (isMovingToStart)
+        private Vector2 startPosition;
+        private bool isMovingToStart = false;
+
+        private void Start()
         {
-            transform.position = Vector2.MoveTowards(transform.position, startPosition, attackSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, startPosition) < 0.1)
+            startPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            if (isMovingToStart)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startPosition, attackSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, startPosition) < 0.1)
+                {
+                    isMovingToStart = false;
+                }
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
             {
                 isMovingToStart = false;
+                transform.position = Vector2.MoveTowards(transform.position, player.position, attackSpeed * Time.deltaTime);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
+            {
+                isMovingToStart = true;
             }
         }
     }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if(collision.tag == "Player")
-        {
-            isMovingToStart = false;
-            transform.position = Vector2.MoveTowards(transform.position, player.position, attackSpeed * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            isMovingToStart = true;
-        }
-    }
-}
 }
